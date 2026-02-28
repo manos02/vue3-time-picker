4e83f6305dcdb0a27a470c9852997b54cf927bfb
manos02 <64806766+manos02@users.noreply.github.com>
2026-02-28 20:56:02 +0100

    chore: remove storybook-static from repository

--- storybook-static/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as TimePicker } from './TimePicker/TimePicker.vue';
-export { TimePicker };
-export type { DisabledTimeInput, TimeFormat, InternalFormat, TimePickerProps, ValidationReason, ValidationState, } from './TimePicker/types';
